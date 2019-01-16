c5cdd4bb1def89e79a331d8dd6d787aca575d17c
douweschepers <douwe.schepers@student.hu.nl>
2019-01-16 23:00:46 +0100

    Update BaseDao.java

--- src/main/java/pandanew/daos/BaseDao.java
+++ src/main/java/pandanew/daos/BaseDao.java
@@ -18,9 +18,9 @@ public class BaseDao{
 		}
 		
 		try{
-			connection = DriverManager.getConnection("jdbc:postgresql://ec2-79-125-4-96.eu-west-1.compute.amazonaws.com:5432/d1cf9u1unakubt?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory",
-					"fdssbigrhdsikk",
-					"a7a7e09f12ef66bf6a01844d1b190940405f643dc559df68824cf22be30c8bbf");
+			connection = DriverManager.getConnection("jdbc:postgresql://ec2-79-125-4-96.eu-west-1.compute.amazonaws.com:5432/ddcue537ga2q9o?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory",
+					"apfknhnihnpjun",
+					"d4c9ce51634fbe93c050a829c5799bba8f44254058543b4f251326f2ba321d7d");
 		}catch (SQLException e) {
 			e.printStackTrace();
 
